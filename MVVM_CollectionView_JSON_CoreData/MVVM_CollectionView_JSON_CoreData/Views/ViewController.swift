3bc8c272cd2f9a042c9fffa967a3e57be36e793a
Dmitrii Kim <mitya_kim@icloud.com>
2024-12-01 22:03:42 -0800

    add new MVVM project with UICollectionView and JSON example

---
+++ MVVM_CollectionView_JSON_CoreData/MVVM_CollectionView_JSON_CoreData/Views/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MVVM_CollectionView_JSON_CoreData
+//
+//  Created by Mitya Kim on 12/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionView()
+        style()
+        layout()
+    }
+}
+
+// MARK: - Helper Methods
+extension ViewController {
+    private func setupCollectionView() {
+        
+    }
+    
+    private func style() {
+        
+    }
+    
+    private func layout() {
+        
+        
+    }
+}
+
