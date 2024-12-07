f648cd29e7944726ddd613e1f3947734a6907e77
Dmitrii Kim <mitya_kim@icloud.com>
2024-12-06 22:06:45 -0800

    add snapshot, UICollectionViewDiffableDataSource

--- GHFollowers/GHFollowers/Models/Follower.swift
+++ GHFollowers/GHFollowers/Models/Follower.swift
@@ -8,12 +8,16 @@
 import Foundation
 
 
-struct Follower: Codable {
+struct Follower: Codable, Hashable {
     let login: String
     let avatarURL: String
     
     enum CodingKeys: String, CodingKey {
         case login
         case avatarURL = "avatar_url"
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(login)
     }
 }
--- GHFollowers/GHFollowers/Screens/FollowerListViewController.swift
+++ GHFollowers/GHFollowers/Screens/FollowerListViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class FollowerListViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
 
     var username: String!
+    private var followers: [Follower] = []
     private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +24,7 @@ class FollowerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         configureCollectionView()
-        configureViewController()
-        
+        configureDataSource()
         getFollowers()
     }
     
@@ -29,37 +34,53 @@ class FollowerListViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-    
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createFlowLayout())
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemRed
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.id)
     }
     
-    private func configureViewController() {
+    private func createFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let itemSpacing: CGFloat = 10
+        let availableWidth = width - padding*2 - itemSpacing*2
+        let itemWidth = availableWidth / 3
         
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
     }
     
     private func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
             switch result {
             case .success(let followers):
-                print(followers)
+                self?.followers = followers!
+                self?.updateData()
             case .failure(let error):
                 self?.presentGFAlertOnMainThread(title: "Error", message: error.localizedDescription, buttonTitle: "OK")
             }
         }
     }
     
-    private func createFlowLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 8
-        let itemSpacing: CGFloat = 8
-        
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        
-        return flowLayout
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: {
+            (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.id, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    private func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
     }
 }
